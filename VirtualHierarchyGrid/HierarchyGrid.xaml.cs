07030e11dd6543cd210d87c287f5ac3cb65a0fd3
Demortier Jeremy <jeremy.demortier@nbb.be>
2021-05-17 11:08:45 +0200

    Fix #8

--- VirtualHierarchyGrid/HierarchyGrid.xaml.cs
+++ VirtualHierarchyGrid/HierarchyGrid.xaml.cs
@@ -24,135 +24,146 @@ namespace VirtualHierarchyGrid
         {
             InitializeComponent();
 
-            this.WhenActivated(disposables =>
-            {
-                this.WhenAnyValue(x => x.ViewModel)
-                    .WhereNotNull()
-                    .Do(vm => PopulateFromViewModel(this, vm, disposables))
-                    .SubscribeSafe()
-                    .DisposeWith(disposables);
-            });
+            this.WhenActivated( disposables =>
+             {
+                 this.WhenAnyValue( x => x.ViewModel )
+                     .WhereNotNull()
+                     .Do( vm => PopulateFromViewModel( this , vm , disposables ) )
+                     .SubscribeSafe()
+                     .DisposeWith( disposables );
+             } );
         }
 
-        private static void PopulateFromViewModel(HierarchyGrid hierarchyGrid, HierarchyGridViewModel vm, CompositeDisposable disposables)
+        private static void PopulateFromViewModel( HierarchyGrid view , HierarchyGridViewModel viewModel , CompositeDisposable disposables )
         {
-            vm.DrawGridInteraction.RegisterHandler(ctx =>
+            viewModel.DrawGridInteraction.RegisterHandler( ctx =>
             {
-                vm.Log().Debug("Drawing grid");
-                hierarchyGrid.DrawGrid(hierarchyGrid.RenderSize);
-                ctx.SetOutput(Unit.Default);
-            })
-                .DisposeWith(disposables);
+                viewModel.Log().Debug( "Drawing grid" );
+                view.DrawGrid( view.RenderSize );
+                ctx.SetOutput( Unit.Default );
+            } )
+                .DisposeWith( disposables );
 
-            vm.EndEditionInteraction.RegisterHandler(ctx =>
+            viewModel.EndEditionInteraction.RegisterHandler( ctx =>
             {
-                var textBoxes = hierarchyGrid.HierarchyGridCanvas.Children.OfType<TextBox>().ToArray();
-                foreach (var tb in textBoxes)
+                var textBoxes = view.HierarchyGridCanvas.Children.OfType<TextBox>().ToArray();
+                foreach ( var tb in textBoxes )
                 {
-                    if (tb.Tag is IDisposable d)
+                    if ( tb.Tag is IDisposable d )
                         d.Dispose();
-                    hierarchyGrid.HierarchyGridCanvas.Children.Remove(tb);
+                    view.HierarchyGridCanvas.Children.Remove( tb );
                 }
 
-                ctx.SetOutput(Unit.Default);
-            })
-                .DisposeWith(disposables);
+                ctx.SetOutput( Unit.Default );
+            } )
+                .DisposeWith( disposables );
 
-            vm.EditInteraction.RegisterHandler(ctx =>
+            viewModel.EditInteraction.RegisterHandler( ctx =>
             {
                 var (rowIdx, colIdx, resultSet) = ctx.Input;
 
-                var cell = hierarchyGrid.HierarchyGridCanvas.Children.OfType<HierarchyGridCell>()
-                    .FirstOrDefault(o => o.ViewModel.RowIndex == rowIdx && o.ViewModel.ColumnIndex == colIdx);
+                var cell = view.HierarchyGridCanvas.Children.OfType<HierarchyGridCell>()
+                    .FirstOrDefault( o => o.ViewModel.RowIndex == rowIdx && o.ViewModel.ColumnIndex == colIdx );
 
-                if (cell != null)
+                if ( cell != null )
                 {
                     var textBox = new TextBox();
                     textBox.Width = cell.Width;
                     textBox.Height = cell.Height;
-                    Canvas.SetLeft(textBox, Canvas.GetLeft(cell));
-                    Canvas.SetTop(textBox, Canvas.GetTop(cell));
-                    hierarchyGrid.HierarchyGridCanvas.Children.Add(textBox);
+                    Canvas.SetLeft( textBox , Canvas.GetLeft( cell ) );
+                    Canvas.SetTop( textBox , Canvas.GetTop( cell ) );
+                    view.HierarchyGridCanvas.Children.Add( textBox );
 
-                    textBox.Tag = textBox.Events().KeyDown.Subscribe(e =>
+                    textBox.Tag = textBox.Events().KeyDown.Subscribe( e =>
                     {
-                        if (e.Key == Key.Enter || e.Key == Key.Return)
-                            if (resultSet.Editor.Match(edt => edt(textBox.Text), () => false))
-                                Observable.Return(Unit.Default)
-                                    .Delay(TimeSpan.FromMilliseconds(50))
-                                    .InvokeCommand(vm, x => x.BuildResultSetsCommand);
-                        if (e.Key == Key.Escape || e.Key == Key.Enter || e.Key == Key.Return)
-                            vm.IsEditing = false;
-                    });
+                        if ( e.Key == Key.Enter || e.Key == Key.Return )
+                            if ( resultSet.Editor.Match( edt => edt( textBox.Text ) , () => false ) )
+                                Observable.Return( Unit.Default )
+                                    .Delay( TimeSpan.FromMilliseconds( 50 ) )
+                                    .InvokeCommand( viewModel , x => x.BuildResultSetsCommand );
+                        if ( e.Key == Key.Escape || e.Key == Key.Enter || e.Key == Key.Return )
+                            viewModel.IsEditing = false;
+                    } );
 
                     textBox.Focus();
                 }
 
-                ctx.SetOutput(Unit.Default);
-            })
-                .DisposeWith(disposables);
+                ctx.SetOutput( Unit.Default );
+            } )
+                .DisposeWith( disposables );
 
-            hierarchyGrid.Background = (Brush)hierarchyGrid.TryFindResource("GridBackground") ?? Brushes.LightGray;
-            hierarchyGrid.Corner.Fill = (Brush)hierarchyGrid.TryFindResource("GridBackground") ?? Brushes.LightGray;
+            view.Background = (Brush) view.TryFindResource( "GridBackground" ) ?? Brushes.LightGray;
+            view.Corner.Fill = (Brush) view.TryFindResource( "GridBackground" ) ?? Brushes.LightGray;
 
-            hierarchyGrid.OneWayBind(vm,
-                vm => vm.Scale,
-                v => v.ScaleTransform.ScaleX)
-                .DisposeWith(disposables);
+            view.OneWayBind( viewModel ,
+                vm => vm.Scale ,
+                v => v.ScaleTransform.ScaleX )
+                .DisposeWith( disposables );
 
-            hierarchyGrid.OneWayBind(vm,
-                vm => vm.Scale,
-                v => v.ScaleTransform.ScaleY)
-                .DisposeWith(disposables);
+            view.OneWayBind( viewModel ,
+                vm => vm.Scale ,
+                v => v.ScaleTransform.ScaleY )
+                .DisposeWith( disposables );
 
-            hierarchyGrid.HierarchyGridCanvas.Events()
+            view.HierarchyGridCanvas.Events()
                 .SizeChanged
-                .Throttle(TimeSpan.FromMilliseconds(75))
-                .ObserveOn(RxApp.MainThreadScheduler)
-                .SubscribeSafe(e =>
+                .Throttle( TimeSpan.FromMilliseconds( 75 ) )
+                .ObserveOn( RxApp.MainThreadScheduler )
+                .SubscribeSafe( e =>
                 {
-                    hierarchyGrid.DrawGrid(e.NewSize);
-                })
-                .DisposeWith(disposables);
+                    view.DrawGrid( e.NewSize );
+                } )
+                .DisposeWith( disposables );
 
-            hierarchyGrid.HierarchyGridCanvas.Events()
+            view.HierarchyGridCanvas.Events()
                 .MouseWheel
-                .SubscribeSafe(e =>
+                .SubscribeSafe( e =>
                 {
-                    if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
-                        vm.Scale += .05 * (e.Delta < 0 ? 1 : -1);
-                    else if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
-                        vm.HorizontalOffset += 5 * (e.Delta < 0 ? 1 : -1);
+                    if ( Keyboard.IsKeyDown( Key.LeftCtrl ) || Keyboard.IsKeyDown( Key.RightCtrl ) )
+                        viewModel.Scale += .05 * ( e.Delta < 0 ? 1 : -1 );
+                    else if ( Keyboard.IsKeyDown( Key.LeftShift ) || Keyboard.IsKeyDown( Key.RightShift ) )
+                        viewModel.HorizontalOffset += 5 * ( e.Delta < 0 ? 1 : -1 );
                     else
-                        vm.VerticalOffset += 5 * (e.Delta < 0 ? 1 : -1);
-                })
-                .DisposeWith(disposables);
+                        viewModel.VerticalOffset += 5 * ( e.Delta < 0 ? 1 : -1 );
+                } )
+                .DisposeWith( disposables );
 
-            hierarchyGrid.Bind(vm,
-                vm => vm.HorizontalOffset,
-                v => v.HorizontalScrollBar.Value,
-                hierarchyGrid.HorizontalScrollBar.Events().Scroll,
-                vmToViewConverter: i => Convert.ToDouble(i),
-                viewToVmConverter: d => Convert.ToInt32(d))
-                .DisposeWith(disposables);
+            view.Bind( viewModel ,
+                vm => vm.HorizontalOffset ,
+                v => v.HorizontalScrollBar.Value ,
+                view.HorizontalScrollBar.Events().Scroll ,
+                vmToViewConverter: i => Convert.ToDouble( i ) ,
+                viewToVmConverter: d => Convert.ToInt32( d ) )
+                .DisposeWith( disposables );
 
-            hierarchyGrid.Bind(vm,
-                vm => vm.VerticalOffset,
-                v => v.VerticalScrollBar.Value,
-                hierarchyGrid.VerticalScrollBar.Events().Scroll,
-                vmToViewConverter: i => Convert.ToDouble(i),
-                viewToVmConverter: d => Convert.ToInt32(d))
-                .DisposeWith(disposables);
+            view.Bind( viewModel ,
+                vm => vm.VerticalOffset ,
+                v => v.VerticalScrollBar.Value ,
+                view.VerticalScrollBar.Events().Scroll ,
+                vmToViewConverter: i => Convert.ToDouble( i ) ,
+                viewToVmConverter: d => Convert.ToInt32( d ) )
+                .DisposeWith( disposables );
 
-            hierarchyGrid.OneWayBind(vm,
-                vm => vm.MaxHorizontalOffset,
-                v => v.HorizontalScrollBar.Maximum)
-                .DisposeWith(disposables);
+            view.OneWayBind( viewModel ,
+                vm => vm.MaxHorizontalOffset ,
+                v => v.HorizontalScrollBar.Maximum )
+                .DisposeWith( disposables );
 
-            hierarchyGrid.OneWayBind(vm,
-                vm => vm.MaxVerticalOffset,
-                v => v.VerticalScrollBar.Maximum)
-                .DisposeWith(disposables);
+            view.OneWayBind( viewModel ,
+                vm => vm.MaxVerticalOffset ,
+                v => v.VerticalScrollBar.Maximum )
+                .DisposeWith( disposables );
+
+            view.OneWayBind( viewModel ,
+                    vm => vm.HasData ,
+                    v => v.MessageBorder.Visibility ,
+                    b => b ? Visibility.Collapsed : Visibility.Visible )
+                .DisposeWith( disposables );
+
+            view.OneWayBind( viewModel ,
+                    vm => vm.StatusMessage ,
+                    v => v.MessageTextBlock.Text  )
+                .DisposeWith( disposables );
         }
     }
 }
