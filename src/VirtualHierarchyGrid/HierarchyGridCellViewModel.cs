f63f337dbefc6aa3d053db95eebf1f81d04b3024
CyLuGh <jeremy.demortier@gmail.com>
2021-10-22 09:53:58 +0200

    Allow multi level context menu (use pipes)

--- src/VirtualHierarchyGrid/HierarchyGridCellViewModel.cs
+++ src/VirtualHierarchyGrid/HierarchyGridCellViewModel.cs
@@ -29,92 +29,92 @@ namespace VirtualHierarchyGrid
 
         public HierarchyGridViewModel HierarchyGridViewModel { get; }
 
-        public ReactiveCommand<Unit, Unit> ShowContextMenuCommand { get; private set; }
+        public ReactiveCommand<Unit , Unit> ShowContextMenuCommand { get; private set; }
 
-        public Interaction<(string, ICommand)[], Unit> ShowContextMenuInteraction { get; }
-            = new Interaction<(string, ICommand)[], Unit>(RxApp.MainThreadScheduler);
+        public Interaction<(string, ICommand)[] , Unit> ShowContextMenuInteraction { get; }
+            = new Interaction<(string, ICommand)[] , Unit>( RxApp.MainThreadScheduler );
 
-        public HierarchyGridCellViewModel(HierarchyGridViewModel hierarchyGridViewModel)
+        public HierarchyGridCellViewModel( HierarchyGridViewModel hierarchyGridViewModel )
         {
             Activator = new ViewModelActivator();
             HierarchyGridViewModel = hierarchyGridViewModel;
 
-            InitializeCommands(this);
+            InitializeCommands( this );
 
-            this.WhenActivated(disposables =>
-            {
-                ShowContextMenuInteraction
-                    .RegisterHandler(ctx => ctx.SetOutput(Unit.Default))
-                    .DisposeWith(disposables);
+            this.WhenActivated( disposables =>
+             {
+                 ShowContextMenuInteraction
+                     .RegisterHandler( ctx => ctx.SetOutput( Unit.Default ) )
+                     .DisposeWith( disposables );
 
-                HierarchyGridViewModel
-                    .WhenAnyValue(x => x.HoveredRow)
-                    .CombineLatest(HierarchyGridViewModel.WhenAnyValue(x => x.HoveredColumn),
-                    hierarchyGridViewModel.WhenAnyValue(x => x.EnableCrosshair),
-                    (row, col, ec) => (row, col, ec))
-                    .SubscribeSafe(t =>
-                    {
-                        var (row, col, ec) = t;
-                        if (ec)
-                            IsHovered = row == RowIndex || col == ColumnIndex;
-                    })
-                    .DisposeWith(disposables);
+                 HierarchyGridViewModel
+                     .WhenAnyValue( x => x.HoveredRow )
+                     .CombineLatest( HierarchyGridViewModel.WhenAnyValue( x => x.HoveredColumn ) ,
+                     hierarchyGridViewModel.WhenAnyValue( x => x.EnableCrosshair ) ,
+                     ( row , col , ec ) => (row, col, ec) )
+                     .SubscribeSafe( t =>
+                     {
+                         var (row, col, ec) = t;
+                         if ( ec )
+                             IsHovered = row == RowIndex || col == ColumnIndex;
+                     } )
+                     .DisposeWith( disposables );
 
-                hierarchyGridViewModel.Highlights.Connect()
-                    .ObserveOn(RxApp.MainThreadScheduler)
-                    .SubscribeSafe(_ =>
-                    {
-                        IsHighlighted = hierarchyGridViewModel.Highlights.Items
-                                            .Any(x => (x.isRow && x.pos == RowIndex) || (!x.isRow && x.pos == ColumnIndex));
-                    })
-                    .DisposeWith(disposables);
+                 hierarchyGridViewModel.Highlights.Connect()
+                     .ObserveOn( RxApp.MainThreadScheduler )
+                     .SubscribeSafe( _ =>
+                     {
+                         IsHighlighted = hierarchyGridViewModel.Highlights.Items
+                                             .Any( x => ( x.isRow && x.pos == RowIndex ) || ( !x.isRow && x.pos == ColumnIndex ) );
+                     } )
+                     .DisposeWith( disposables );
 
-                hierarchyGridViewModel.SelectedPositions.Connect()
-                    .SubscribeSafe(_ =>
-                    {
-                        IsSelected = hierarchyGridViewModel.SelectedPositions.Lookup((RowIndex, ColumnIndex)).HasValue;
-                    })
-                    .DisposeWith(disposables);
+                 hierarchyGridViewModel.SelectedPositions.Connect()
+                     .SubscribeSafe( _ =>
+                     {
+                         IsSelected = hierarchyGridViewModel.SelectedPositions.Lookup( (RowIndex, ColumnIndex) ).HasValue;
+                     } )
+                     .DisposeWith( disposables );
 
-                this.WhenAnyValue(x => x.ResultSet)
-                    .CombineLatest(this.WhenAnyValue(x => x.IsHovered),
-                    this.WhenAnyValue(x => x.IsHighlighted),
-                    (rs, ho, hi) => (rs, ho, hi))
-                    .Select(t =>
-                    {
-                        var (resultSet, isHovered, isHighlighted) = t;
-                        if (resultSet == null)
-                            return Qualification.Empty;
-                        return isHovered ? Qualification.Hovered
-                            : (isHighlighted ? Qualification.Highlighted : resultSet.Qualifier);
-                    })
-                    .ObserveOn(RxApp.MainThreadScheduler)
-                    .ToPropertyEx(this, x => x.Qualifier)
-                    .DisposeWith(disposables);
+                 this.WhenAnyValue( x => x.ResultSet )
+                     .CombineLatest( this.WhenAnyValue( x => x.IsHovered ) ,
+                     this.WhenAnyValue( x => x.IsHighlighted ) ,
+                     ( rs , ho , hi ) => (rs, ho, hi) )
+                     .Select( t =>
+                     {
+                         var (resultSet, isHovered, isHighlighted) = t;
+                         if ( resultSet == null )
+                             return Qualification.Empty;
+                         return isHovered ? Qualification.Hovered
+                             : ( isHighlighted ? Qualification.Highlighted : resultSet.Qualifier );
+                     } )
+                     .ObserveOn( RxApp.MainThreadScheduler )
+                     .ToPropertyEx( this , x => x.Qualifier )
+                     .DisposeWith( disposables );
 
-                this.WhenAnyValue(x => x.ResultSet)
-                    .Select(rs =>
-                    {
-                        if (rs == null)
-                            return false;
+                 this.WhenAnyValue( x => x.ResultSet )
+                     .Select( rs =>
+                     {
+                         if ( rs == null )
+                             return false;
 
-                        return rs.Editor.Match(_ => rs.Qualifier != Qualification.ReadOnly, () => false);
-                    })
-                    .ObserveOn(RxApp.MainThreadScheduler)
-                    .ToPropertyEx(this, x => x.CanEdit)
-                    .DisposeWith(disposables);
-            });
+                         return rs.Editor.Match( _ => rs.Qualifier != Qualification.ReadOnly , () => false );
+                     } )
+                     .ObserveOn( RxApp.MainThreadScheduler )
+                     .ToPropertyEx( this , x => x.CanEdit )
+                     .DisposeWith( disposables );
+             } );
         }
 
-        private static void InitializeCommands(HierarchyGridCellViewModel @this)
+        private static void InitializeCommands( HierarchyGridCellViewModel @this )
         {
             @this.ShowContextMenuCommand =
-                ReactiveCommand.CreateFromObservable(() =>
+                ReactiveCommand.CreateFromObservable( () =>
                 {
-                    var commands = @this.ResultSet.ContextCommands.Some(o => o)
-                        .None(() => new (string, ICommand)[0]);
-                    return @this.ShowContextMenuInteraction.Handle(commands);
-                });
+                    var commands = @this.ResultSet.ContextCommands.Some( o => o )
+                        .None( () => new (string, ICommand)[0] );
+                    return @this.ShowContextMenuInteraction.Handle( commands );
+                } );
         }
 
         internal void Clear()
